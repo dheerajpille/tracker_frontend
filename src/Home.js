74da175b3f0ebbeabed9b209e23117c0ab493f00
dheerajpille <dheerajpille@gmail.com>
2017-11-15 14:47:11 -0500

    Scaled banner to window size

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import banner from './banner.png'
+import banner from './banner.png';
 import './App.css';
 
 class Home extends Component {
@@ -13,23 +13,39 @@ class Home extends Component {
                     </div>
                     <div className="navbar">
                         <ul>
-                            <li><h3>Overview</h3></li>
-                            <li><h3>Login</h3></li>
+                            <li><h3><a>Login</a></h3></li>
                         </ul>
                     </div>
                 </div>
                 <br />
                 <div className="content">
-                    <code>python manage.py makemigrations</code>
-                    <h2>Create your own ideas</h2>
-
+                    <img src={banner} />
+                    <h1 class="banner-title">Spend less, track more</h1>
+                    <h3 class="banner-subtitle">Track your expenses with Tracker, and enjoy life a little more.</h3>
+                    <hr />
+                    <h2>Overview</h2>
+                    <p>
+                        Tracker is a small-scale web application created to efficiently track your expenses. Now, you
+                        don't need to spend hours searching for bills or determining whether you surpassed your allotted
+                        monthly budget on video games. Instead, just add your expenses to Tracker, and get a report on
+                        what you spent your money on.
+                    </p>
+                    <p>
+                        Simple and easy.
+                    </p>
+                    <hr />
+                    <h2>Details</h2>
+                    <p>
+                        Tracker operates by integrating a React front-end and a Django back-end together to create this
+                        fully functional web application. User and expense data is stored within a PostgreSQL database
+                        that are accessed via API calls.
+                    </p>
                     <p className="App-intro">
                         To get started, edit <code>src/App.js</code> and save to reload.
                     </p>
-                    <p>Find time for yourself</p>
+                    <p>check out www.github.com</p>
                 </div>
                 <br />
-                <footer>Created by Dheeraj Pille</footer>
             </div>
         );
     }
