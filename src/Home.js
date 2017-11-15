89c13a3f81f75cb884a609cfc33730fda06ae840
dheerajpille <dheerajpille@gmail.com>
2017-11-15 10:56:43 -0500

    Added banner

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import banner from './banner.png'
 import './App.css';
 
 class Home extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
+                <div class="header">
                     <h1>Tracker</h1>
-                    <h2>An intuitive expense tracker built for modern times</h2>
-                </header>
-                <h1>
-                    Tracker
-                </h1>
-
+                </div>
+                <img id={banner} src={banner} />
                 <code>python manage.py makemigrations</code>
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
+                <footer>Created by Dheeraj Pille</footer>
             </div>
         );
     }
