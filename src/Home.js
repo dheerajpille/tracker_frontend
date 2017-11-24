c4c284aa75a4cc9fcd618bcd71a8cd236f8c7616
dheerajpille <dheerajpille@gmail.com>
2017-11-24 07:11:10 -0500

    Changed primary font from Roboto to Lato

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import banner from './banner.png';
 import './Home.css';
 
 class Home extends Component {
@@ -13,33 +12,17 @@ class Home extends Component {
                         <h1>Tracker</h1>
                     </div>
                 </div>
-
                 <br />
-
                 <div className="content">
-                    <h2>Overview</h2>
-                    <p>
-                        Tracker is a small-scale web `application created to efficiently track your expenses. Now, you
-                        don't need to spend hours searching for bills or determining whether you surpassed your allotted
-                        monthly budget on video games. Instead, just add your expenses to Tracker, and get a report on
-                        what you spent your money on.
-                    </p>
-                    <p>
-                        Simple and easy. Just the way you'd like it.
-                    </p>
-                    <hr />
-                    <h2>Details</h2>
-                    <p>
-                        Tracker operates by integrating a React front-end and a Django back-end together to create this
-                        fully functional web application. User and expense data is stored within a PostgreSQL database
-                        that are accessed via API calls.
-                    </p>
-                    <p className="App-intro">
-                        To get started, edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <p>check out www.github.com</p>
+                    <div className="content-title">
+                        <h2>Track more. Worry less.</h2>
+                    </div>
+                    <div className="content-body">
+                        <h2>Waste no time finding your expenses.</h2>
+                        <p>Track with Tracker and preserve your sanity.</p>
+
+                    </div>
                 </div>
-                <br />
             </div>
         );
     }
